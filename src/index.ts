9235f653a8be230acfb93b4337da8584bd26f719
Markus Moltke <markus@unknown-studios.com>
2023-12-16 16:52:39 +0100

    :bug: fix: wrong HTTP method for fetching cookie policy

--- src/index.ts
+++ src/index.ts
@@ -125,7 +125,7 @@ export const insertCookiePolicy = async () => {
       const response = await fetch(
         'https://green-analytics.com/api/database/cookie-policy',
         {
-          method: 'POST',
+          method: 'GET',
           headers: {
             'Content-Type': 'application/json',
 
